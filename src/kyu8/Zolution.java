ff9bf84e4566ab9f36e66a02092d02118125c412
DHIRAJB7 <dhiraj.basavaraju@gmail.com>
2022-09-11 15:06:34 +0530

    change in IDE

--- src/kyu8/Zolution.java
+++ src/kyu8/Zolution.java
@@ -1,11 +1,11 @@
 package kyu8;
 
 public class Zolution {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("I AM ZOLUTION");
-		
-	}
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD AGAIN!!!..............LOL");
+
+    }
 
 }
