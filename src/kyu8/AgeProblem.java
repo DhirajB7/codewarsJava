18bdd63b6dca50757d0f2df772f92369fff7af7b
DHIRAJB7 <dhiraj.basavaraju@gmail.com>
2022-10-13 08:22:56 +0530

    ++

---
+++ src/kyu8/AgeProblem.java
@@ -0,0 +1,9 @@
+package kyu8;
+
+public class AgeProblem {
+    public static int howOld(final String herOld) {
+
+        return  Integer.parseInt(herOld.split(" ")[0]);
+
+    }
+}
---
+++ src/kyu8/FakeBinary.java
@@ -0,0 +1,17 @@
+package kyu8;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class FakeBinary {
+
+    public static String fakeBin(String numberString) {
+       return Arrays.stream(numberString.split("")).filter(a -> a.length()==1).map(a -> Integer.parseInt(a) < 5 ? "0" : "1" ).collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fakeBin("45385593107843568"));
+        System.out.println(fakeBin(""));
+    }
+
+}
