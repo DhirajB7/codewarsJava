eadaaa92befe0323fa76ff4b72c793a0322adfd3
dhiraj <dhiraj@dhiraj-20V9>
2023-05-24 15:24:23 +0530

    ++ changes

---
+++ src/kyu7/Minimum.java
@@ -0,0 +1,22 @@
+package kyu7;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Minimum {
+	
+	public static int minValue(int[] values){
+		
+		 List<String> stringValues = Arrays.stream(values).distinct().boxed().sorted()
+				 .map(a->String.valueOf(a)).toList();
+		
+		return Integer.parseInt(String.join("", stringValues));
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(minValue(new int[] {6, 7, 8, 7, 6, 6}));
+		
+	}
+
+}
