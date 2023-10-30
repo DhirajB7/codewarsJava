6e9f78e5586268a53c7f266f1e9aa9b141cf58ae
Dhiraj <dhiraj.basavaraju@gmail.com>
2023-10-31 04:26:25 +0530

    ++

--- src/codewars/Zolution.java
+++ src/codewars/Zolution.java
@@ -1,11 +1,13 @@
 package codewars;
 
+import java.util.ArrayList;
 
 public class Zolution {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World !!!");
+ 
+		System.out.println("*".repeat(10));
+		ArrayList<Integer> al = new ArrayList<Integer>();
 		
 	}
 
